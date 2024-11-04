6d99527b2f1f294fb001e8ee955ba0e52b5a4b6e
alireza rahmani <150823639+alireza-rahmani991@users.noreply.github.com>
2024-11-04 19:22:51 +0330

    hal soalat

--- fasl3/soal2.cpp
+++ fasl3/soal2.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 int combination(int n, int r){
+    if(r > n){
+        return 0;
+    }
     int* arr = new int[r + 1];
     arr[0] = 1;
     for(int i = 1; i <= r; i++){
         
         arr[i] =(arr[i - 1] * (n - r + i)) / i;
     }
-    return arr[r];
+    int res = arr[r];
+    delete [] arr;
+    return res;
 }
 
 
